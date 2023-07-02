510d495857a25ba1092cd07888b183e0761e1c1d
P <ypariyar@a07817ac7524.ant.amazon.com>
2023-07-02 10:07:33 -0500

    Push

--- webappv1/app.js
+++ webappv1/app.js
@@ -8,8 +8,7 @@ var con = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 3306
+  database: process.env.DB_NAME
 })
 
 con.connect(function(err){curl 
